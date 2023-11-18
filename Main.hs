ba3d4217b70c7080c9da30b65bb181ea9ed2b7e1
Sam Anklesaria <anklesaria@utexs.edu>
2023-11-18 15:16:22 -0600

    Add fancy labels

--- Main.hs
+++ Main.hs
@@ -7,19 +7,24 @@ import Chain
 import Patricia
 import PatChain (PatChain)
 import Msg
-import Graphics.Rendering.Chart.Easy hiding (Vector)
+import Graphics.Rendering.Chart.Easy hiding (Vector, colors, shapes)
 import Graphics.Rendering.Chart.Backend.Cairo
 import Data.Vector.Unboxed (Vector)
 import qualified Data.Vector.Unboxed as V
 import Control.Monad
 import Control.DeepSeq
 import Data.List (foldl')
+import Control.Monad.State
 
 materialize = V.fromList . uncurry zip
 
 test v b a st = materialize <$> simulate st v b a 100 200
 
 parallel xs = foldl' (flip par) xs xs
+
+shapes = [PointShapeCircle, PointShapePlus, PointShapeCross]
+
+colors = map opaque [aliceblue, orangered, green]
 
 testAll :: (forall a. Msg a => a -> IO (Vector (Double, Double)))
   -> IO [Vector (Double, Double)]
@@ -34,9 +39,13 @@ theTests = sequence [
 main = do
   setNumCapabilities 27
   results <- theTests
-  forM_ (zip ["delay", "gossip", "send"] results) $ \(l, st)-> do
+  forM_ (zip ["Time Skew Scale", "Gossip Scale", "Sending Scale"] results) $ \(l, st)-> do
     toFile def (l ++ ".png") $ do
-      forM_ (zip ["0.5", "1", "2"] st) $ \(val, strat)-> do
-        forM_ (zip ["patricia", "chain", "patchain"] strat) $ \(lbl, f)->
+      modify $ (layout_x_axis . laxis_title .~ "seconds") . (layout_y_axis . laxis_title .~ "nodes examined")
+      layout_title .= l ++ " Comparison"
+      forM_ (zip3 ["0.5", "1", "2"] st shapes) $ \(val, strat, sh)-> do
+        setShapes [sh]
+        forM_ (zip3 ["patricia", "chain", "patchain"] strat colors) $ \(lbl, f, c)-> do
+          setColors [c]
           plot $ points (lbl ++ " " ++ val) $ V.toList f
 
